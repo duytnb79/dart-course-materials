6563b8c4f531d026ba62a1d84730ed6d823a2386
duy <tranngocbaoduy88@gmail.com>
2024-08-10 08:50:49 +0700

    first lesson from dart ^^

---
+++ answer/02.09-quiz-string-escaping.dart
@@ -0,0 +1,19 @@
+// ## Quiz: String escaping
+
+// Can you guess what's the output of the following code, without running it?
+
+// ```dart
+// void main() {
+//   int a = 10;
+//   print('$a');
+//   print('\$a');
+//   print('\$$a');  
+// }
+// ```
+
+
+// Output is 
+
+// 10
+// $a
+// $10
---
+++ answer/02.13-lowercase-uppercase.dart
@@ -0,0 +1,23 @@
+
+// ## Exercise: Lowercase and Uppercase
+
+// Using only one variable defined like this:
+
+// ```dart
+// String title = 'Dart course';
+// ```
+
+// Write a program that uses `title` to produce the following output:
+
+// ```
+// Dart course
+// DART COURSE
+// dart course
+// ```
+
+void main(){
+    String title = 'Dart course';
+    print(title);
+    print(title.toUpperCase());
+    print(title.toLowerCase());
+}
